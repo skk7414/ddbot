2d63be4832c53d120dfba27226ee3a4e3d41a301
skk7414 <skk7414naver.com>
2020-12-26 10:10:07 +0900

    bot commit

--- index.js
+++ index.js
@@ -18,46 +18,46 @@ client.on('messageDelete', async message => {
   
   //뮤트
 
-let Cooltime_Mute = 3 * 1000 //밀리세컨드 
-// 2초내에 칠 시 뮤트
-let User_Mute_Object = {}
-client.on('message', async message => {
-  let MuteRole = client.guilds.cache.get(message.guild.id).roles.cache.find(r => r.name === "Muted").id
-  if (message.author.bot || !message.guild) return
-  MuteRole = message.guild.roles.cache.find(r => r.id == MuteRole)
-  const M_Author = message.author
-  if (!message.member.hasPermission('ADMINISTRATOR')) {
-    let Author_Object = User_Mute_Object[M_Author.id]
-    if (!Author_Object) {
-      User_Mute_Object[M_Author.id] = {
-        time: 0,
-        interval: null,
-        muted: false
-      }
-    } else {
-      if (Author_Object.interval != null) {
-        if (Cooltime_Mute >= Author_Object.time && !Author_Object.muted) {
-          message.member.roles.add(MuteRole)
-          Author_Object.muted = true
-          message.reply(`전 채팅과의 시간차 ${Author_Object.time}ms`)
+  let Cooltime_Mute = 2 * 1000 //밀리세컨드 
+  // 2초내에 칠 시 뮤트
+  let User_Mute_Object = {}
+  client.on('message', async message => {
+    let MuteRole = client.guilds.cache.get(message.guild.id).roles.cache.find(r => r.name === "Muted").id
+    if (message.author.bot || !message.guild) return
+    MuteRole = message.guild.roles.cache.find(r => r.id == MuteRole)
+    const M_Author = message.author
+    if (!message.member.hasPermission('ADMINISTRATOR')) {
+      let Author_Object = User_Mute_Object[M_Author.id]
+      if (!Author_Object) {
+        User_Mute_Object[M_Author.id] = {
+          time: 0,
+          interval: null,
+          muted: false
         }
-        clearInterval(Author_Object.interval)
-        Author_Object.interval = null
-      } else if (!Author_Object.muted) {
-        Author_Object.interval = setInterval(() => {
-          Author_Object.time++
-        }, 1)
+      } else {
+        if (Author_Object.interval != null) {
+          if (Cooltime_Mute >= Author_Object.time && !Author_Object.muted) {
+            message.member.roles.add(MuteRole)
+            Author_Object.muted = true
+            message.reply('전 채팅과의 시간차 ${Author_Object.time}ms')
+          }
+          clearInterval(Author_Object.interval)
+          Author_Object.interval = null
+        } else if (!Author_Object.muted) {
+          Author_Object.interval = setInterval(() => {
+            Author_Object.time++
+          }, 1)
+        }
+        Author_Object.time = 0
       }
-      Author_Object.time = 0
     }
-  }
-  if (message.member.hasPermission('ADMINISTRATOR') && /!언뮤트 <@!?(\d{17,19})>/g.test(message.content)) {
-    const Mention_member = message.mentions.members.first()
-    Mention_member.roles.remove(MuteRole)
-    User_Mute_Object[Mention_member.id].muted = false
-    User_Mute_Object[Mention_member.id].time = 0
-    message.channel.send(`${Mention_member}, 해방됨`)
-  }
-})
+    if (message.member.hasPermission('ADMINISTRATOR') && /!언뮤트 <@!?(\d{17,19})>/g.test(message.content)) {
+      const Mention_member = message.mentions.members.first()
+      Mention_member.roles.remove(MuteRole)
+      User_Mute_Object[Mention_member.id].muted = false
+      User_Mute_Object[Mention_member.id].time = 0
+      message.channel.send('${Mention_member}, 해방됨')
+    }
+  })
 
 client.login(token); // node index.js 를 쳐서 봇에 로그인하기!
